76c18c68fee4c40ed077bdfe743dc4ef9b8ef325
Ramiro Gumma <ramirogumma@hotmail.com>
2023-07-03 11:41:35 -0300

    Add E.commerce-Coderhouse briefcase

--- src/components/Carousel-brief.jsx
+++ src/components/Carousel-brief.jsx
@@ -22,6 +22,12 @@ import watherapp from "../../public/screen.png";
 import devops from "../../public/DevOps News 1.png";
 import appproduct from "../../public/appproduct.png";
 import commerce from "../../public/commerce.jpeg";
+import node from "../../public/node.png";
+import mongo from "../../public/MongoDb.png";
+import express from "../../public/express.png";
+import JWTlogo from "../../public/JWT.png";
+import eCommerceCoder from "../../public/e-commerce-coderhouse.PNG";
+import stripe from "../../public/stripe.png";
 
 export default function Carrouselbrief(props) {
   const { lenguajes } = props;
@@ -678,6 +684,145 @@ export default function Carrouselbrief(props) {
                   >
                     <a
                       href="https://e-commerce-rsn-hamipluf.vercel.app/"
+                      target="BLANK"
+                    >
+                      {lenguajes.go}
+                    </a>
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full p-4 py-10 mx-10 space-x-9 carousel carousel-center rounded-box">
+        {/* MODAl-7 */}
+        <div className="carousel-item">
+          <div className="">
+            {/* OPEN MODAL-7 */}
+            <label htmlFor="my-modal-7" className="cursor-pointer modal-button">
+              <div className="hover:shadow-xl rounded-box hover:shadow-infor hover:translate-x-6 hover:transition-all hover:origin-top-left hover:translate-y-6">
+                <Image
+                  width={370}
+                  height={200}
+                  src={eCommerceCoder}
+                  alt="Imagen de portafolio web creado Por Ramiro Gumma"
+                  className="rounded-box"
+                />
+              </div>
+            </label>
+            {/* BODY MODAL-7 */}
+            <input type="checkbox" id="my-modal-7" className="modal-toggle" />
+            <div className="text-white modal">
+              <div className="modal-box bg-gradient-to-b from-dark to-obscure ">
+                <label
+                  htmlFor="my-modal-7"
+                  className="absolute btn btn-sm btn-circle right-2 top-2 bg-infor text-dark hover:scale-125 hover:bg-dark hover:text-infor"
+                >
+                  ✕
+                </label>
+                <h3 className="text-lg font-bold text-infor ">
+                  E-Commerce CoderHouse{`/>`}
+                </h3>
+                <p className="py-4">
+                  {lenguajes.carrouselM8}
+                  <br />
+                  <br />
+                  <span className="text-xl text-infor">|</span>{" "}
+                  <span>{lenguajes.carrouselSubtitle}</span>
+                  <br />
+                  <br />
+                  {lenguajes.development}:
+                </p>
+                <hr />
+                <div className="grid grid-cols-3 py-5 justify-items-center gap-y-5">
+                  <div>
+                    <Image
+                      width={50}
+                      height={50}
+                      priority
+                      src={html}
+                      alt="logo diseñado por josefina ritter de html"
+                    />
+                  </div>
+                  <div>
+                    <Image
+                      width={50}
+                      height={50}
+                      priority
+                      src={css}
+                      alt="logo diseñado por josefina ritter de css"
+                    />
+                  </div>
+                  <div>
+                    <Image
+                      width={50}
+                      height={50}
+                      priority
+                      src={js}
+                      alt="logo diseñado por josefina ritter de javascript"
+                    />
+                  </div>
+                  <div>
+                    <Image
+                      width={40}
+                      height={50}
+                      priority
+                      src={stripe}
+                      alt="logo diseñado por josefina ritter de react"
+                    />
+                  </div>
+                  <div>
+                    <Image
+                      width={50}
+                      height={50}
+                      priority
+                      src={node}
+                      alt="logo diseñado por josefina ritter de tailwind"
+                    />
+                  </div>
+                  <div>
+                    <Image
+                      width={50}
+                      height={50}
+                      priority
+                      src={mongo}
+                      alt="logo diseñado por josefina ritter de verecel"
+                    />
+                  </div>
+                  <div>
+                    <Image
+                      width={50}
+                      height={50}
+                      priority
+                      src={express}
+                      alt="logo diseñado por josefina ritter de verecel"
+                    />
+                  </div>
+                  <div>
+                    <Image
+                      width={50}
+                      height={50}
+                      priority
+                      src={JWTlogo}
+                      alt="logo diseñado por josefina ritter de verecel"
+                    />
+                  </div>
+                </div>
+                <div className="border-2 rounded-md border-infor">
+                  <Image
+                    responive
+                    src={eCommerceCoder}
+                    alt="Imagen de portafolio web creado Por Ramiro Gumma"
+                  />
+                </div>
+                <div className="modal-action">
+                  <label
+                    htmlFor="my-modal"
+                    className="btn bg-infor text-dark hover:scale-125 hover:bg-dark hover:text-infor"
+                  >
+                    <a
+                      href="https://e-commercecoderhouse-production.up.railway.app/"
                       target="BLANK"
                     >
                       {lenguajes.go}
